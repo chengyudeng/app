dbe0c769809019886f0bb3794ed42c15b7b0f71c
chengyudeng <1456694385@qq.com>
2018-06-25 01:45:20 +0800

    登录注册， 用户信息校验， 跳转登录

--- src/index.js
+++ src/index.js
@@ -16,12 +16,17 @@ const store = createStore(reducers, compose(
     window.devToolsExtension ? window.devToolsExtension() : f => f
 ));
 
+function Boss () {
+    return <h2>boss</h2>;
+}
+
 
 ReactDOM.render(
     (<Provider store={ store }>
         <BrowserRouter>
             <div>
                 <AuthRoute></AuthRoute>
+                <Route path='/boss' component={ Boss }></Route>
                 <Route path='/login' component={ Login }></Route>
                 <Route path='/register' component={ Register }></Route>
             </div>
