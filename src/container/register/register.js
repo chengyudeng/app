4af7ffe0a495e8d087673859de398e497d4e4d18
chengyudeng <1456694385@qq.com>
2018-06-25 23:17:42 +0800

    登录注册, 注册交互实现

--- src/container/register/register.js
+++ src/container/register/register.js
@@ -6,8 +6,20 @@ export default class Register extends Component {
     constructor (props) {
         super(props);
         this.state = {
+            user: '',
+            pwd: '',
+            repeatpwd: '',
             type: 'genius'
-        }
+        };
+        this.handleRegister = this.handleRegister.bind(this);
+    }
+    handleChange(key, val) {
+        this.setState({
+            [key]: val
+        });
+    }
+    handleRegister () {
+        console.log(this.state);
     }
     render () {
         const RadioItem = Radio.RadioItem;
@@ -15,20 +27,32 @@ export default class Register extends Component {
             <div>
                 <Logo />
                 <List>
-                    <InputItem>用户</InputItem>
+                    <InputItem 
+                        onChange={ v => this.handleChange('user', v) }
+                    >用户</InputItem>
                     <WhiteSpace />
-                    <InputItem>密码</InputItem>
+                    <InputItem
+                        type="password"
+                        onChange={ v => this.handleChange('pwd', v) }
+                    >密码</InputItem>
                     <WhiteSpace />
-                    <InputItem>确认密码</InputItem>
+                    <InputItem
+                        type="password"
+                        onChange={ v => this.handleChange('repeatpwd', v) }
+                    >确认密码</InputItem>
                     <WhiteSpace />
-                    <RadioItem checked={ this.state.type === 'genius' }>
+                    <RadioItem checked={ this.state.type === 'genius' }
+                        onChange={ () => this.handleChange('type', 'genius') }
+                    >
                         牛人
                     </RadioItem>
-                    <RadioItem checked={ this.state.type === 'boss' }>
+                    <RadioItem checked={ this.state.type === 'boss' }
+                        onChange={ () => this.handleChange('type', 'boss') }
+                    >
                         boss
                     </RadioItem>
                     <WhiteSpace />
-                    <Button type='primary'>注册</Button>
+                    <Button type='primary' onClick={ this.handleRegister }>注册</Button>
                 </List>
                 <h2>注册页</h2>
             </div>
