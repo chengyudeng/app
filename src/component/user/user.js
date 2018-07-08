83a8da580addc50f73aa627bcda872f38692594b
chengyudeng <1456694385@qq.com>
2018-07-08 14:07:13 +0800

    个人中心信息展示2

--- src/component/user/user.js
+++ src/component/user/user.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Result, List, WhiteSpace } from 'antd-mobile';
 
 @connect(
-    state => state
+    state => state.user
 )
 export default class User extends Component {
     render () {
-        console.log(this.props);
-        return (
+        const props = this.props;
+        const Item = List.Item;
+        const Brief = Item.Brief;
+        return props.user ? (
             <div>
-                <p>用户中心页</p>
+                <Result 
+                    img={ <img src={ require(`../img/${props.avatar}.png`) } style={ { width: 50 } } alt='' />  }
+                    title={ props.user }
+                    message={ props.type === 'boss' ? props.company : null }
+                />
+                <List renderHeader={ () => '简介' }>
+                    <Item
+                        multipleLine
+                    >
+                        { props.title }
+                        { props.desc.split('\n').map(v => <Brief key={v}>{ v }</Brief>) }
+                        { props.money ? <Brief>薪资: { props.money }</Brief> : null }
+                    </Item>
+                </List>
+                <WhiteSpace />
+                <List>
+                    <Item>退出登录</Item>
+                </List>
             </div>
-        );
+        ) : null;
     }
 }
