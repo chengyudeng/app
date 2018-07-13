ec8c98e567742373f150dbee1301262296ebf757
chengyudeng <1456694385@qq.com>
2018-07-13 21:45:35 +0800

    使用imoocForm高阶组件优化代码

--- src/container/login/login.js
+++ src/container/login/login.js
@@ -4,6 +4,7 @@ import { List, InputItem, WingBlank, WhiteSpace, Button } from 'antd-mobile';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../redux/user.redux';
+import imoocForm from '../../component/imooc-form/imooc-form';
 
 // function hello () {
 //     console.log('hello imooc, i love react');
@@ -22,26 +23,28 @@ import { login } from '../../redux/user.redux';
 // hello();
 
 // 属性代理
-function WrapperHello (Comp) {
-    class WrapComp extends Component {
-        render () {
-            return (
-                <div>
-                    <p>这是HOC高阶组件特有的元素</p>
-                    <Comp { ...this.props }></Comp>
-                </div>
-            )
-        }
-    }
-    return WrapComp;
-}
+// function WrapperHello (Comp) {
+//     class WrapComp extends Comp {
+//         componentDidMount () {
+//             console.log('高阶组件新增的生命周期');
+//         }
+//         render () {
+//             return <Comp></Comp>
+//         }
+//     }
+//     // class WrapComp extends Component {
+//     //     render () {
+//     //         return (
+//     //             <div>
+//     //                 <p>这是HOC高阶组件特有的元素</p>
+//     //                 <Comp name='text' { ...this.props }></Comp>
+//     //             </div>
+//     //         )
+//     //     }
+//     // }
+//     return WrapComp;
+// }
 
-@WrapperHello
-class Hello extends Component {
-    render () {
-        return <h2>hello chengyudeng I love React & Redux</h2>
-    }
-}
 
 // Hello = WrapperHello(Hello)
 
@@ -49,42 +52,33 @@ class Hello extends Component {
     state => state.user,
     { login }
 )
+@imoocForm
 export default class Login extends Component {
     constructor (props) {
         super(props);
-        this.state = {
-            user: '',
-            pwd: ''
-        }
         this.register = this.register.bind(this);
         this.handleLogin = this.handleLogin.bind(this);
     }
     register () {
         this.props.history.push('/register');
     }
-    handleChange (key, val) {
-        this.setState({
-            [key]: val
-        });
-    }
     handleLogin () {
-        this.props.login(this.state);
+        this.props.login(this.props.state);
     }
     render () {
         return (
             <div>
-                <Hello></Hello>
                 { this.props.redirectTo && this.props.redirectTo !== '/login' ? <Redirect to={ this.props.redirectTo } /> : null } 
                 <Logo />
                 <WingBlank>
                     <List>
                         { this.props.msg ? <p className="error-msg">{ this.props.msg }</p> : null }
                         <InputItem
-                            onChange={ v => this.handleChange('user', v) }
+                            onChange={ v => this.props.handleChange('user', v) }
                         >用户</InputItem>
                         <WhiteSpace />
                         <InputItem
-                            onChange={ v => this.handleChange('pwd', v) }
+                            onChange={ v => this.props.handleChange('pwd', v) }
                             type='password'
                         >密码</InputItem>
                     </List>
